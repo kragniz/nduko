2a34566752c0d03425b4a7ca70bfeb0204d5ddb8
Louis Taylor <kragniz@gmail.com>
2011-10-10 04:45:21 -0400

    started writing exact cover

---
+++ exactCover/linkedList.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+class LinkedList(object):
+    def __main__(self):
+        pass
+    
+class Node(object):
+    def __main__(self):
+        pass
+
+if __name__ == '__main__':
+    print 'doing something cool with linked lists'
