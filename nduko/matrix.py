d49aabb6517c85d33a173090be528d72b6813cd5
Louis Taylor <kragniz@gmail.com>
2011-10-11 07:55:06 -0400

    wrote some more of Matrix

--- nduko/matrix.py
+++ nduko/matrix.py
@@ -1,8 +1,17 @@
 class Matrix(object):
     def __init__(self):
-        dataStructure = {}
+        self.dataStructure = {}
+        
+    def setItem(self, r, c, value):
+        try:
+            self.dataStructure[c][r] = value 
+        except KeyError:
+            self.dataStructure[c] = {r:value}
+
     def test(self):
         pass 
 
 if __name__ == '__main__':
-    m = Matrix
+    m = Matrix()
+    #sets the item in row 0, column 0 to 1
+    m.setItem(0, 0, 1)
