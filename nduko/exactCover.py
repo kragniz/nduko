71685c7808441ed5af8e4b0807ea6ed7402973c6
Louis Taylor <kragniz@gmail.com>
2011-10-19 09:10:01 -0400

    fixed a merge conflict

--- nduko/exactCover.py
+++ nduko/exactCover.py
@@ -11,21 +11,35 @@ class ExactCover(object):
 
     def solve(self, A):
         '''Solve the exact cover problem on the matrix A'''
+        #If the matrix A is empty, the problem is solved; terminate
+        #successfully.
         if len(A) == 0:
             #we have completed the problem, return the result
             return A
         else:
+<<<<<<< Updated upstream
             solution = []
             
+=======
+            #Otherwise choose a column c (deterministically).
+>>>>>>> Stashed changes
             c = A.column(A.columns()[0])
             rows = []
+            #Choose a row r such that A(r, c) = 1
             for r in range(len(c)):
                 if c[r] == 1:
                     print self.matrix().row(r)
+                    #Include row r in the partial solution.
                     rows += [r]
+<<<<<<< Updated upstream
                     
             if len(rows):
                 solution = self.matrix().row(choice(rows))
+=======
+            print choice(rows)
+            #For each column j such that A(r, j) = 1,
+            for j in 
+>>>>>>> Stashed changes
                         
     def matrix(self):
         return self._matrix
