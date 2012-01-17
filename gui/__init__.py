cc65d9e82d02cc989bf948668aad413e93ad8ffc
Louis Taylor <kragniz@gmail.com>
2012-01-17 12:53:51 +0000

    built some more of the gui. Added a menubar.

--- gui/__init__.py
+++ gui/__init__.py
@@ -20,14 +20,20 @@ class gui(object):
         builder.add_from_file(gladeFile)
         builder.connect_signals(self)
 
-    def undo(self, widget):
-    	print 'undone'
+    def on_undoButton_clicked(self, widget):
+    	self.undo()
+
+    def on_quitmenuitem_activate(self, widget):
+    	self.quit()
 
     def on_window_destroy(self, widget):
     	self.quit()
 
     def on_quitButton_clicked(self, widget):
     	self.quit()
+
+    def undo(self):
+    	print 'undone'
 
     def quit(self, errorCode=0):
     	print 'quitting with style'
