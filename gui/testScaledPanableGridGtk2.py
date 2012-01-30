3b07c06644fceeee8530ba52fe9768d1152db465
Louis Taylor <kragniz@gmail.com>
2012-01-30 09:15:19 +0000

    Made the widget array close when the main window is destroyed

--- gui/testScaledPanableGridGtk2.py
+++ gui/testScaledPanableGridGtk2.py
@@ -7,11 +7,19 @@ except:
     import gtk as Gtk
     print 'falling back to gtk 2'
 
+import sys
+
 class gui(object):
     def __init__( self ):
         gladeFile = 'testScaledPanableGridGtk2.ui'
         builder = Gtk.Builder()
         builder.add_from_file(gladeFile)
+        builder.connect_signals(self)
+
+    def on_window_destroy(self, event):
+        print 'dying'
+        sys.exit()
+
 
 if __name__ == '__main__':
 	g = gui()
