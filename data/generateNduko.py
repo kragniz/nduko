3af074e9d23335039e16e6717d439c3ff3b97107
Louis Taylor <kragniz@gmail.com>
2012-02-10 00:37:07 +0000

    Wrote the outer loop for the exact cover generator
    Started the inner loop

--- data/generateNduko.py
+++ data/generateNduko.py
@@ -6,8 +6,15 @@ class GenerateNduko(object):
     def __init__(self, n):
         self.output = open('../data/generatedNduko', 'w')
 
-        for i in range(n**2):
-            for j in range(n):
+        cellValue = 0
+        rowOffset = 0
+
+        for i in range(n**4):
+            for j in range(n**2):
+                self.output.write('{0} {1}\n'.format(cellValue, rowOffset))
+            cellValue += 1
+            if not (i % n**4):
+                rowOffset += n**2
 
 if __name__ == '__main__':
-    g = GenerateNduko(25)
+    g = GenerateNduko(2)
