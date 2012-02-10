c248f5037c81c25afbd0af4176d67ec62a9874d9
Louis Taylor <kragniz@gmail.com>
2012-02-10 11:13:00 +0000

    Wrote the first two columns of the exact cover creator
    Also wrote a small function to generate a human readable exact cover problem viewer.

--- data/generateNduko.py
+++ data/generateNduko.py
@@ -4,6 +4,7 @@ import sys, os
 
 class GenerateNduko(object):
     def __init__(self, n):
+        self.n = n
         self.output = open('../data/generatedNduko', 'w')
 
         cellValue = 0
@@ -11,10 +12,23 @@ class GenerateNduko(object):
 
         for i in range(n**4):
             for j in range(n**2):
-                self.output.write('{0} {1}\n'.format(cellValue, rowOffset))
+                self.output.write('{0} {1}\n'.format(cellValue,
+                                                     n**4 + (rowOffset * n**2 + (j % n**2))))
             cellValue += 1
-            if not (i % n**4):
-                rowOffset += n**2
+            if (i+1) % (n**2) == 0:
+                rowOffset += 1
+        self.output.close()
+
+    def writeViewable(self):
+        with open('../data/viewableNduko', 'w') as fileOut:
+            with open('../data/generatedNduko') as fileIn:
+                for line in range(self.n**6):
+                    line = ['-' for i in range((self.n**4)*4)]
+                    cell, row = fileIn.readline()[:-1].split()
+                    line[int(cell)] = '1'
+                    line[int(row)] = '1'
+                    fileOut.write(''.join(line) + '\n')
 
 if __name__ == '__main__':
     g = GenerateNduko(2)
+    g.writeViewable()
